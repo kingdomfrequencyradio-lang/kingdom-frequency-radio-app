00629a7b8b8eb75cb1de2c19b11d377f606f8bc8
kingdomfrequencyradio-lang <kingdomfrequencyradio@gmail.com>
2026-07-11 12:11:01 -0400

    Delete service-worker 2.js

--- service-worker 2.js
+++
@@ -1,9 +0,0 @@
-const CACHE='kfr-v2';
-const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./logo.jpg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{
-  const u=new URL(e.request.url);
-  if(u.hostname.includes('zeno.fm')) return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))));
-});
